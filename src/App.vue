7520e7c8df7f2ba38ede3f2a73879ad3622afdb2
dnk92 <aaze92@gmail.com.com>
2024-01-13 23:47:41 -0600

    agrego optimizaciones para mostrar/ocultar

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@ import { defineProps, watch, reactive } from 'vue';
 import Nunca from './components/Nunca.vue';
 import Siempre from './components/Siempre.vue';
 
+
+const ver = reactive({
+  mostrarSiempre: false,
+  mostrarNunca: false
+});
+
 const verSiempre = reactive({
   mostrar: false
 });
@@ -11,30 +17,41 @@ const verNunca = reactive({
 });
 
 const mostrarSiempre = () => {
-  verSiempre.mostrar = true;
+  ver.mostrarSiempre = true;
 };
 const mostrarNunca = () => {
-  verNunca.mostrar = true;
+  ver.mostrarNunca = true;
 };
 const ocultarSiempre = () => {
-  verSiempre.mostrar = false;
+  ver.mostrarSiempre = false;
 };
 const ocultarNunca = () => {
-  verNunca.mostrar = false;
+  ver.mostrarNunca = false;
 };
 
 </script>
 
 <template>
-  
-  <header>
-   <a @click="mostrarSiempre" >SIEMPRE</a>
-    <Siempre v-if="verSiempre.mostrar"/>
-  </header>
+  <!--   <header>
+
+  </header> -->
 
   <main>
-   <a @click="mostrarNunca">NUNCA</a>
-    <Nunca v-if="verNunca.mostrar"/>
+    <h1>¿Qué dijiste?</h1>
+    <div class="split left">
+      <div class="centered">
+        <a @click="mostrarSiempre" v-if="!ver.mostrarSiempre">SIEMPRE</a>
+        <Siempre v-if="ver.mostrarSiempre" />
+        <a @click="ocultarSiempre" v-if="ver.mostrarSiempre">Ocultar Siempre</a>
+      </div>
+    </div>
+    <div class="split right">
+      <div class="centered">
+        <a @click="mostrarNunca" v-if="!ver.mostrarNunca">NUNCA</a>
+        <Nunca v-if="ver.mostrarNunca" />
+        <a @click="ocultarNunca" v-if="ver.mostrarNunca">Ocultar Nunca</a>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -64,5 +81,28 @@ header {
     place-items: flex-start;
     flex-wrap: wrap;
   }
+}
+
+.split {
+  height: 100%;
+  width: 50%;
+  position: fixed;
+  /* position: fixed;
+  z-index: 1;
+  top: 0;
+  overflow-x: hidden;
+  padding-top: 20px; */
+}
+/* Control the left side */
+.left {
+  left: 50%;
+  align-self: self-end;
+  /*background-color: #111;*/
+}
+
+/* Control the right side */
+.right {
+  right: 50%;
+  /*background-color: red;*/
 }
 </style>
--- src/components/Nunca.vue
+++ src/components/Nunca.vue
@@ -5,17 +5,18 @@ const fechaActual = new Date().toISOString().slice(0, 10);
   <div>
     <h1>NUNCA</h1>
     <p>¿Qué dijiste que nunca harías?</p>
-    <form @submit.prevent="submit" >
-    <h2><label for="fecha">Fecha:</label></h2>
-        <input type="date" id="fecha" readonly :value="fechaActual">
-        <br>
-    <h2><label for="descripcion">Descripcion</label></h2>
-        <textarea name="descripcion" id="descripcion" cols="30" rows="10" placeholder="Ingresa descipción de lo que dijiste como NUNCa"></textarea>
-        <br>
-    <h2><label for="motivo">Motivo</label></h2>
-        <input type="text" id="motivo" placeholder="Ingresa el motivo por el cual dijiste NUNCA">
-        <br>
-    <button type="submit">NUNCA</button>
-</form>
+    <form @submit.prevent="submit">
+      <h2><label for="fecha">Fecha:</label></h2>
+      <input type="date" id="fecha" readonly :value="fechaActual">
+      <br>
+      <h2><label for="descripcion">Descripcion</label></h2>
+      <textarea name="descripcion" id="descripcion" cols="30" rows="10"
+        placeholder="Ingresa descipción de lo que dijiste como NUNCA"></textarea>
+      <br>
+      <h2><label for="emocion">Emociones</label></h2>
+      <input type="text" id="emocion" placeholder="Ingresa la emoción mientras lo decias.">
+      <br>
+      <button type="submit">NUNCA</button>
+    </form>
   </div>
 </template>
